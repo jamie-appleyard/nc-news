bcee6074b5aa706cd68e6a24cb1483162a4a33af
Jamie <jappleyardp@gmail.com>
2024-02-19 15:23:52 +0000

    Completed get article by id endpoint with error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -26,6 +26,44 @@ describe('/api/topics', () => {
     })
 });
 
+describe('/api/articles/:article_id', () => {
+    test('GET 200: returns the article with the article_id given as a parameter in the URL', () => {
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then((response) => {
+            const {article} = response.body
+            expect(article).toMatchObject({
+                title: expect.any(String),
+                topic: expect.any(String),
+                author: expect.any(String),
+                body: expect.any(String),
+                created_at: expect.any(String),
+                votes: expect.any(Number),
+                article_img_url: expect.any(String)
+            })
+        })
+    });
+    test('GET 400: returns an appropriate status and message when passed a valid id that does not exist', () => {
+        return request(app)
+        .get('/api/articles/999')
+        .expect(400)
+        .then((response) => {
+            expect(response.body.status).toBe(400)
+            expect(response.body.msg).toBe('ID does\'nt exist')
+        })
+    });
+    test('GET 404: returns appropriate status and message when given an invalid ID as a parameter', () => {
+        return request(app)
+        .get('/api/articles/myarticle')
+        .expect(404)
+        .then((response) => {
+            expect(response.body.status).toBe(404)
+            expect(response.body.msg).toBe('Invalid ID parameter')
+        })
+    })
+});
+
 describe('/api', () => {
     test('GET 200: returns a JSON object representing the the available endpoints of the API and their functionality', () => {
         return request(app)
